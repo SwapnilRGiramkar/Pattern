1b764cb32f87ae97de8cd58179c6727779865f21
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2025-07-16 21:32:26 +0530

    This s Standing Rectangle

---
+++ StandingRectangle.java
@@ -0,0 +1,17 @@
+//This Program gives a Standing Rectangle 
+// StandingRectangle.java
+class StandRec
+{
+	public static void main(String[] args)
+	{
+		int n=5;
+		for(int j=0;j<5;j++)
+		{
+			for(int i=0;i<n-1;i++)
+			{
+				System.out.print("* "+" ");
+			}
+			System.out.println();
+		}
+	}
+}
