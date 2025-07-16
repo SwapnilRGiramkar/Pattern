0643d481fac5cd92a8262ffea03a88d8a3a2bf42
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2025-07-16 21:23:31 +0530

    Square Pattern

---
+++ SquarePattern.java
@@ -0,0 +1,16 @@
+//This program prints the Square Pattern 
+class Pattern1
+{
+	public static void main(String[] args) 
+	{
+		int n=5;
+		for (int j=0; j<5 ; j++) 
+		{
+			for (int i=0;i<n ;i++ ) 
+			{
+				System.out.print("* ");
+			}
+			System.out.println();	
+		}
+	}
+}
