e93aa55162147f6650f0ecc702ae27c2b8860ded
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2025-07-16 22:16:59 +0530

    This Program gives Cross

---
+++ Cross.java
@@ -0,0 +1,25 @@
+//This program gives us Cross
+class Cross
+{
+	public static void main(String[] args) 
+	{
+		int n=5;
+		for(int i=0 ;i <n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==j || i+j==n-1 )
+				{
+					System.out.print("* ");
+				}
+				else 
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
+
+
